7b6528e4f8dacd474329fae21cba98590b292fb4
afternun <baoslabel@gmail.com>
2018-10-25 08:12:13 +0000

    New Test

--- creditcard-app/src/main/java/pl/mbal/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/mbal/creditcard/CreditCard.java
@@ -1,11 +1,19 @@
 package pl.mbal.creditcard;
 
 class CreditCard {
+    
+    boolean status = true;
+    
     public void assignLimit(double money) {
         
     }
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void block()
+    {
+        this.status = false;
     }
 }
