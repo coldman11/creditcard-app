f9d657795c1363e37d6251ca0164169189d84c31
Michal <miketopolsky1@interia.pl>
2018-11-14 19:08:49 +0100

    added withdraw feature

--- creditcard-app/src/main/java/pl/mbal/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/mbal/creditcard/CreditCard.java
@@ -1,90 +1,29 @@
 package pl.mbal.creditcard;
 
-import java.math.BigDecimal;
-
 class CreditCard {
     
-    private boolean blocked = false;
-    private BigDecimal limit;
-    private BigDecimal balance = BigDecimal.ZERO;
-    private String id;
+    boolean blocked = false;
+    int limit;
     
-    public CreditCard(String id)
-    {
-        this.id = id;
-    }
-    public CreditCard()
-    {
-        this.id = "random_string";
-    }
-    
-    public String getId()
-    {
-        return id;
-    }
-    
-    public void assignLimit(BigDecimal money) {
-        if (isLimitAlreadyAsigned()) {
-            throw new LimitAlreadyAssignedException();
-        }
-
-        if (isLimitBelowOrEquals0(money)) {
-            throw new InsufficientCreditLimitException();
-        }
-
+    public void assignLimit(double money) {
         this.limit = money;
-        this.balance = money;
     }
-
-    private boolean isLimitBelowOrEquals0(BigDecimal money) {
-        return money.compareTo(BigDecimal.ZERO) <= 0;
-    }
-
-    private boolean isLimitAlreadyAsigned() {
-        return limit != null;
-    }
-
-    public BigDecimal getLimit() {
+    
+    public double getLimit() {
         return limit;
     }
     
-    public void block() {
+    public void block()
+    {
         this.blocked = true;
     }
     
-    public boolean isBlocked() {
-        return this.blocked;
+    public boolean isBlocked()
+    {
+        return blocked;
     }
-
-    public void withdraw(BigDecimal money) {
-        if (isWithdrawOverTheLimit(money))
-            throw new NotEnoughMoneyException();
-
-        if (isNotEnoughMoney(money))
-            throw new NotEnoughMoneyException();
-
-        if (isBlocked())
-            throw new TransactionOnBlockedCardException();
-
-        balance = balance.subtract(money);
-    }
-
-    private boolean isNotEnoughMoney(BigDecimal money) {
-        return money.compareTo(balance) > 0;
-    }
-
-    private boolean isWithdrawOverTheLimit(BigDecimal money) {
-        return money.compareTo(limit) > 0;
-    }
-
-    public void repay(BigDecimal money) {
-        if (money.compareTo(BigDecimal.ZERO) < 0) {
-            throw new CantRepayNegativeAmountException();
-        }
-        balance = balance.add(money);
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
+       public void withdraw(int money)
+    {
+        this.limit = this.limit - money;
     }
 }
